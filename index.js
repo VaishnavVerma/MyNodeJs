56a32cc9adb1cf23f7b338aaa881fb89c4a39048
VaishnavVerma <vaishnavverma0@gmail.com>
2024-01-06 11:56:56 +0530

    push changes

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url"); // Include the 'url' module
 
 const myServer = http.createServer((req, res) => {
+    const myUrl = url.parse(req.url, true); // Move this line inside the server callback
+
     const log = `${Date.now()}: ${req.url} New request received\n`;
-    fs.appendFile("log.txt", log, (err, data) => {
-        switch (req.url) {
-            case '/':
-                res.end("HomePage");
-                break;
-            case '/about':
-                res.end("About page");
-                break;
-            default:
-                res.end("Hello server, I'm Vaishanv verma");
+    fs.appendFile("log.txt", log, (err) => {
+        if (err) {
+            console.error("Error writing to log file:", err);
         }
     });
+
+    switch (myUrl.pathname) {
+        case '/':
+            res.end("HomePage");
+            break;
+
+        case '/search':
+            const search = myUrl.query.search_query;
+            res.end("Here are search result for " + search);
+        case '/about':
+            const username = myUrl.query.myname;
+            res.end(`About page. Username: ${username}`);
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end("Error 404: Page not found");
+    }
+
     console.log(req.url);
 });
 
